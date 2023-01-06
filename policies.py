6f2338d29edc98877a6a0c1552402bc0ce7a7661
unknown <alexlove1818@gmail.com>
2023-01-06 20:36:58 +1100

    Adding exchange addresses

--- policies.py
+++ policies.py
@@ -11,3 +11,11 @@ policy_id_big = "11ff0e0d9ad037d18e3ed575cd35a0513b8473f83008124db89f1d8f" # 11k
 
 list_policy_id = [policy_id_aramar, policy_id_atsuko, policy_id_daisuke, policy_id_fourth]
 list_policy_id_test = [policy_id_small, policy_id_small1, policy_id_small2]
+
+stake_jpg = "stake1uxqh9rn76n8nynsnyvf4ulndjv0srcc8jtvumut3989cqmgjt49h6"
+stake_jpg1 = "addr1w999n67e86jn6xal07pzxtrmqynspgx0fwmcmpua4wc6yzsxpljz3"
+stake_cnft = "addr1w9yr0zr530tp9yzrhly8lw5upddu0eym3yh0mjwa0qlr9pgmkzgv0"
+stake_cnft1 = "addr1w89s3lfv7gkugker5llecq6x3k2vjvfnvp4692laeqe6w6s93vj3j"
+stake_epoch = "addr1wyd3phmr5lhv3zssawqjdpnqrm5r5kgppmmf7864p3dvdrqwuutk4"
+stake_jam = "addr1w8e28gp8dlw9a7pcs2ae7cmuww8gp9t8z6vm93ft9uflkpg8netpd"
+stake_cardahub = "addr1wxx0w0ku3jz8hz5dakg982lh22xx6q7z2z7vh0dt34uzghqrxdhqq"
